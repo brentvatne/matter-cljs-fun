4c5f18da47077b6a18efc32149922b6ff7a4f2af
Brent Vatne <brent.vatne@madriska.com>
2015-01-21 17:46:26 -0800

    Better styles

--- out/matter_cljs_fun/core.cljs
+++ out/matter_cljs_fun/core.cljs
@@ -14,16 +14,9 @@
 (def Constraint (.-Constraint Matter))
 (def Composites (.-Composites Matter))
 
-;; Initialize the engine
-(def engine-options
-  {:render {:options {:showAngleIndicator false
-                      :wireframes false}}})
-(def engine
-  (.create Engine (.-body js/document) (clj->js engine-options)))
-(def world
-  (.-world engine))
+(def scene-width 595)
+(def scene-height 600)
 
-;; Set up the pins
 (def pin-cols 25)
 (def pin-rows 19)
 (def start-x 0)
@@ -32,15 +25,24 @@
 (def row-space 28)
 (def col-space 28)
 
+;; Initialize the engine
+(def engine-options
+  {:render {:options {:showAngleIndicator false
+                      :wireframes false
+                      :width scene-width
+                      :height scene-height}}})
+(def engine
+  (.create Engine (.-body js/document) (clj->js engine-options)))
+(def world
+  (.-world engine))
+
+;; Set up the pins
+
 (defn position->pin-black [x y col row]
     (.circle Bodies x y pin-radius #js {:isStatic true :render #js {:fillStyle "#000000"}}))
 
 (defn position->pin-grey [x y col row]
     (.circle Bodies x y pin-radius #js {:isStatic true :render #js {:fillStyle "#cccccc"}}))
-
-; (defn position->pin [x y col row]
-;   (let [offset-x ((if (odd? row) (+ x (/ 2 row-space)) x))]
-;     (.circle Bodies offset-x y pin-radius #js {:isStatic true})))
 
 (def pin-stack
   (.stack Composites start-x start-y pin-cols (/ pin-rows 2) row-space (* 2 col-space) position->pin-black))
@@ -48,24 +50,10 @@
 (def pin-stack-offset
   (.stack Composites (+ start-x (/ row-space 1.25)) (+ (+ col-space (/ pin-radius 1)) start-y) pin-cols (/ pin-rows 2) row-space (* 2 col-space) position->pin-grey))
 
-;; Add the ball
+;; The moving pieces
 (def ball-radius 9)
 (def ball
   (.circle Bodies 300 0 ball-radius #js {:density 0.000001 :frictionAir 0.001}))
-
-;; Set up some basics
-(def ground
-  (.rectangle Bodies 400 595 800 10 #js {:isStatic true}))
-
-(def left-wall
-  (.rectangle Bodies 0 300 10 600 #js {:isStatic true}))
-
-(def right-wall
-  (.rectangle Bodies 800 300 10 600 #js {:isStatic true}))
-
-;; Actually run it
-(.add World world #js [pin-stack pin-stack-offset ground left-wall right-wall ball])
-(.run Engine engine)
 
 (defn add-ball [x]
   (let [new-ball (.circle Bodies x 0 ball-radius #js {:density 0.000001 :frictionAir 0.001})]
@@ -73,7 +61,21 @@
       (.add World world #js [new-ball])))
 
 (defn add-random-ball []
-  (add-ball (* 800 (.random js/Math))))
+  (add-ball (* scene-width (.random js/Math))))
+
+;; Set up the borders
+(def ground
+  (.rectangle Bodies (/ scene-width 2) scene-height scene-width 2 #js {:isStatic true}))
+
+(def left-wall
+  (.rectangle Bodies 0 (/ scene-height 2) 2 scene-height #js {:isStatic true}))
+
+(def right-wall
+  (.rectangle Bodies scene-width (/ scene-height 2) 2 scene-height #js {:isStatic true}))
+
+;; Actually run it
+(.add World world #js [pin-stack pin-stack-offset ground left-wall right-wall ball])
+(.run Engine engine)
 
 (defn add-click-handlers []
   (.addEventListener (.getElementById js/document "add-ball")
--- out/matter_cljs_fun/core.js
+++ out/matter_cljs_fun/core.js
@@ -13,9 +13,8 @@ matter_cljs_fun.core.Body = matter_cljs_fun.core.Matter.Body;
 matter_cljs_fun.core.Bodies = matter_cljs_fun.core.Matter.Bodies;
 matter_cljs_fun.core.Constraint = matter_cljs_fun.core.Matter.Constraint;
 matter_cljs_fun.core.Composites = matter_cljs_fun.core.Matter.Composites;
-matter_cljs_fun.core.engine_options = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"render","render",-1408033454),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"options","options",99638489),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"showAngleIndicator","showAngleIndicator",-1460751489),false,new cljs.core.Keyword(null,"wireframes","wireframes",-1841146676),false], null)], null)], null);
-matter_cljs_fun.core.engine = matter_cljs_fun.core.Engine.create(document.body,cljs.core.clj__GT_js.call(null,matter_cljs_fun.core.engine_options));
-matter_cljs_fun.core.world = matter_cljs_fun.core.engine.world;
+matter_cljs_fun.core.scene_width = (595);
+matter_cljs_fun.core.scene_height = (600);
 matter_cljs_fun.core.pin_cols = (25);
 matter_cljs_fun.core.pin_rows = (19);
 matter_cljs_fun.core.start_x = (0);
@@ -23,6 +22,9 @@ matter_cljs_fun.core.start_y = (10);
 matter_cljs_fun.core.pin_radius = (5);
 matter_cljs_fun.core.row_space = (28);
 matter_cljs_fun.core.col_space = (28);
+matter_cljs_fun.core.engine_options = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"render","render",-1408033454),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"options","options",99638489),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"showAngleIndicator","showAngleIndicator",-1460751489),false,new cljs.core.Keyword(null,"wireframes","wireframes",-1841146676),false,new cljs.core.Keyword(null,"width","width",-384071477),matter_cljs_fun.core.scene_width,new cljs.core.Keyword(null,"height","height",1025178622),matter_cljs_fun.core.scene_height], null)], null)], null);
+matter_cljs_fun.core.engine = matter_cljs_fun.core.Engine.create(document.body,cljs.core.clj__GT_js.call(null,matter_cljs_fun.core.engine_options));
+matter_cljs_fun.core.world = matter_cljs_fun.core.engine.world;
 matter_cljs_fun.core.position__GT_pin_black = (function position__GT_pin_black(x,y,col,row){
 return matter_cljs_fun.core.Bodies.circle(x,y,matter_cljs_fun.core.pin_radius,{"render": {"fillStyle": "#000000"}, "isStatic": true});
 });
@@ -33,11 +35,6 @@ matter_cljs_fun.core.pin_stack = matter_cljs_fun.core.Composites.stack(matter_cl
 matter_cljs_fun.core.pin_stack_offset = matter_cljs_fun.core.Composites.stack((matter_cljs_fun.core.start_x + (matter_cljs_fun.core.row_space / 1.25)),((matter_cljs_fun.core.col_space + (matter_cljs_fun.core.pin_radius / (1))) + matter_cljs_fun.core.start_y),matter_cljs_fun.core.pin_cols,(matter_cljs_fun.core.pin_rows / (2)),matter_cljs_fun.core.row_space,((2) * matter_cljs_fun.core.col_space),matter_cljs_fun.core.position__GT_pin_grey);
 matter_cljs_fun.core.ball_radius = (9);
 matter_cljs_fun.core.ball = matter_cljs_fun.core.Bodies.circle((300),(0),matter_cljs_fun.core.ball_radius,{"frictionAir": 0.001, "density": 1.0E-6});
-matter_cljs_fun.core.ground = matter_cljs_fun.core.Bodies.rectangle((400),(595),(800),(10),{"isStatic": true});
-matter_cljs_fun.core.left_wall = matter_cljs_fun.core.Bodies.rectangle((0),(300),(10),(600),{"isStatic": true});
-matter_cljs_fun.core.right_wall = matter_cljs_fun.core.Bodies.rectangle((800),(300),(10),(600),{"isStatic": true});
-matter_cljs_fun.core.World.add(matter_cljs_fun.core.world,[matter_cljs_fun.core.pin_stack,matter_cljs_fun.core.pin_stack_offset,matter_cljs_fun.core.ground,matter_cljs_fun.core.left_wall,matter_cljs_fun.core.right_wall,matter_cljs_fun.core.ball]);
-matter_cljs_fun.core.Engine.run(matter_cljs_fun.core.engine);
 matter_cljs_fun.core.add_ball = (function add_ball(x){
 var new_ball = matter_cljs_fun.core.Bodies.circle(x,(0),matter_cljs_fun.core.ball_radius,{"frictionAir": 0.001, "density": 1.0E-6});
 console.log(new_ball);
@@ -45,57 +42,62 @@ console.log(new_ball);
 return matter_cljs_fun.core.World.add(matter_cljs_fun.core.world,[new_ball]);
 });
 matter_cljs_fun.core.add_random_ball = (function add_random_ball(){
-return matter_cljs_fun.core.add_ball.call(null,((800) * Math.random()));
+return matter_cljs_fun.core.add_ball.call(null,(matter_cljs_fun.core.scene_width * Math.random()));
 });
+matter_cljs_fun.core.ground = matter_cljs_fun.core.Bodies.rectangle((matter_cljs_fun.core.scene_width / (2)),matter_cljs_fun.core.scene_height,matter_cljs_fun.core.scene_width,(2),{"isStatic": true});
+matter_cljs_fun.core.left_wall = matter_cljs_fun.core.Bodies.rectangle((0),(matter_cljs_fun.core.scene_height / (2)),(2),matter_cljs_fun.core.scene_height,{"isStatic": true});
+matter_cljs_fun.core.right_wall = matter_cljs_fun.core.Bodies.rectangle(matter_cljs_fun.core.scene_width,(matter_cljs_fun.core.scene_height / (2)),(2),matter_cljs_fun.core.scene_height,{"isStatic": true});
+matter_cljs_fun.core.World.add(matter_cljs_fun.core.world,[matter_cljs_fun.core.pin_stack,matter_cljs_fun.core.pin_stack_offset,matter_cljs_fun.core.ground,matter_cljs_fun.core.left_wall,matter_cljs_fun.core.right_wall,matter_cljs_fun.core.ball]);
+matter_cljs_fun.core.Engine.run(matter_cljs_fun.core.engine);
 matter_cljs_fun.core.add_click_handlers = (function add_click_handlers(){
 document.getElementById("add-ball").addEventListener("click",matter_cljs_fun.core.add_random_ball);
 
 return document.getElementById("make-it-rain").addEventListener("click",(function (e){
-var seq__11724 = cljs.core.seq.call(null,cljs.core.range.call(null,(100)));
-var chunk__11725 = null;
-var count__11726 = (0);
-var i__11727 = (0);
+var seq__18947 = cljs.core.seq.call(null,cljs.core.range.call(null,(100)));
+var chunk__18948 = null;
+var count__18949 = (0);
+var i__18950 = (0);
 while(true){
-if((i__11727 < count__11726)){
-var x = cljs.core._nth.call(null,chunk__11725,i__11727);
+if((i__18950 < count__18949)){
+var x = cljs.core._nth.call(null,chunk__18948,i__18950);
 matter_cljs_fun.core.add_random_ball.call(null);
 
-var G__11728 = seq__11724;
-var G__11729 = chunk__11725;
-var G__11730 = count__11726;
-var G__11731 = (i__11727 + (1));
-seq__11724 = G__11728;
-chunk__11725 = G__11729;
-count__11726 = G__11730;
-i__11727 = G__11731;
+var G__18951 = seq__18947;
+var G__18952 = chunk__18948;
+var G__18953 = count__18949;
+var G__18954 = (i__18950 + (1));
+seq__18947 = G__18951;
+chunk__18948 = G__18952;
+count__18949 = G__18953;
+i__18950 = G__18954;
 continue;
 } else {
-var temp__4126__auto__ = cljs.core.seq.call(null,seq__11724);
+var temp__4126__auto__ = cljs.core.seq.call(null,seq__18947);
 if(temp__4126__auto__){
-var seq__11724__$1 = temp__4126__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,seq__11724__$1)){
-var c__4528__auto__ = cljs.core.chunk_first.call(null,seq__11724__$1);
-var G__11732 = cljs.core.chunk_rest.call(null,seq__11724__$1);
-var G__11733 = c__4528__auto__;
-var G__11734 = cljs.core.count.call(null,c__4528__auto__);
-var G__11735 = (0);
-seq__11724 = G__11732;
-chunk__11725 = G__11733;
-count__11726 = G__11734;
-i__11727 = G__11735;
+var seq__18947__$1 = temp__4126__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,seq__18947__$1)){
+var c__4528__auto__ = cljs.core.chunk_first.call(null,seq__18947__$1);
+var G__18955 = cljs.core.chunk_rest.call(null,seq__18947__$1);
+var G__18956 = c__4528__auto__;
+var G__18957 = cljs.core.count.call(null,c__4528__auto__);
+var G__18958 = (0);
+seq__18947 = G__18955;
+chunk__18948 = G__18956;
+count__18949 = G__18957;
+i__18950 = G__18958;
 continue;
 } else {
-var x = cljs.core.first.call(null,seq__11724__$1);
+var x = cljs.core.first.call(null,seq__18947__$1);
 matter_cljs_fun.core.add_random_ball.call(null);
 
-var G__11736 = cljs.core.next.call(null,seq__11724__$1);
-var G__11737 = null;
-var G__11738 = (0);
-var G__11739 = (0);
-seq__11724 = G__11736;
-chunk__11725 = G__11737;
-count__11726 = G__11738;
-i__11727 = G__11739;
+var G__18959 = cljs.core.next.call(null,seq__18947__$1);
+var G__18960 = null;
+var G__18961 = (0);
+var G__18962 = (0);
+seq__18947 = G__18959;
+chunk__18948 = G__18960;
+count__18949 = G__18961;
+i__18950 = G__18962;
 continue;
 }
 } else {
